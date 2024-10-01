3e96766b5d36628b9df813a07af2b24d1b58905b
Rohit Dudi <rohitdudi7023@gmail.com>
2024-10-01 10:56:31 +0530

    array reverse successed without help

--- arrays/reverseSameArray.cpp
+++ arrays/reverseSameArray.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[] ={2,43,65,2,7,3,6,51,1};
+    int arr[] ={2,43,65,2,7,3,6,51};
     int totalIndex = sizeof(arr) / sizeof(int);
     int lastIndex = totalIndex-1;
     int firstIndex = 0;
 
+    if(totalIndex%2 == 0){
+        for(int i = 0 ; i <= totalIndex/2; i++){
+        if (firstIndex+i < lastIndex-i){
+    swap(arr[firstIndex+i], arr[lastIndex-i]);}
+    }
+    }
+    else{
     for(int i = 0 ; i <= totalIndex/2; i++){
     swap(arr[firstIndex+i], arr[lastIndex-i]);
     }
-
-    cout<<"reversed array is: "<<endl;
+   }
+  cout<<"reversed array is: "<<endl;
     for (int i = 0; i < totalIndex; i++){
         cout<<arr[i]<<" ";
     }

25b6f42955db753aa4ec21b84bd721b38174508a
Rohit Dudi <rohitdudijat@yahoo.com>
2024-10-14 10:35:17 +0530

    product of array except self- time & space optimized

--- Algorithms/productArray.cpp
+++ Algorithms/productArray.cpp
@@ -6,18 +6,16 @@ int main(){
     vector<int> nums = {2,3,6,5,3,7};
     int n = nums.size();
     vector<int> ans(n,1);
-    vector<int> prefix(n,1);
-    vector<int> suffix(n,1);
-
+    
     for(int i = 1; i<n; i++){
-        prefix[i] = prefix[i-1] * nums[i-1];
+        ans[i] = ans[i-1] * nums[i-1];
     }
+    int suffix = 1;
     for(int i = n-2; i>=0; i--){
-        suffix[i] = suffix[i+1] * nums[i+1];
+        suffix *= nums[i+1];
+        ans[i] *= suffix;
     }
-    for(int i = 0; i<n; i++){
-        ans[i] = prefix[i] * suffix[i];
-    }
+    
     cout<<"the answer is: "<<endl;
     for(int value : ans){
         cout<<value<<", ";

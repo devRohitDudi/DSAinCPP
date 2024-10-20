1298579c99d67e6013fbcd7210a21fff47666bb4
RohitDudi <rohitdudijat@yahoo.com>
2024-10-20 08:29:20 +0530

    mint setup

--- Algorithms/bestBuyAndSell.cpp
+++ Algorithms/bestBuyAndSell.cpp
@@ -1,16 +1,20 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    vector<int> prices = {7,1,3,6,5,2,4};
-    
+int main()
+{
+    vector<int> prices = {7, 1, 3, 6, 5, 2, 4};
+
     int maxProfit = 0, bestBuy = prices[0];
-    for(int i = 1; i < prices.size(); i++){
-        if(prices[i] > bestBuy){
+    for (int i = 1; i < prices.size(); i++)
+    {
+        if (prices[i] > bestBuy)
+        {
             maxProfit = max(maxProfit, prices[i] - bestBuy);
         }
         bestBuy = min(bestBuy, prices[i]);
     }
-    cout<<"best day to buy stocks is: "<<bestBuy<<endl;
-    cout<<"best day to sell stocks is: "<<maxProfit<<endl;
+    cout << "best day to buy stocks is: " << bestBuy << endl;
+    cout << "best day to sell stocks is: " << maxProfit << endl;
 }
+// connected to github from mint

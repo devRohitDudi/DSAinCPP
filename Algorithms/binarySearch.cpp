331f59ef5c436ff1f368ec6ffd6a6c9bb9a50b6a
RohitDudi <rohitdudijat@yahoo.com>
2024-10-25 08:25:48 +0530

    mid optimization

--- Algorithms/binarySearch.cpp
+++ Algorithms/binarySearch.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
-int BinarySearch(vector<int> arr, int target){
-    int st = arr[0], end = arr.size()-1;
-    while (st<=end)
-    {  int mid = (st+end)/2;
-       if(target < arr[mid]){
-            end = mid-1;
-       }
-       else if(target>arr[mid]){
-         st = mid+1;
-       }
-       else{
-         return mid;
-       }
+int BinarySearch(vector<int> arr, int target)
+{
+    int st = arr[0], end = arr.size() - 1;
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (target < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if (target > arr[mid])
+        {
+            st = mid + 1;
+        }
+        else
+        {
+            return mid;
+        }
     }
     return -1;
 }
-int main(){
+int main()
+{
     int target = 7;
-    vector<int> arrOdd = {-2,0,3,6,7,8,9};
-    vector<int> arrEven = {-2,0,3,7,8,9,12,15};
-    
-    int ans = BinarySearch(arrEven,target);
-    cout<<"the target "<<target<<" is exists at "<<ans<<"th Index on array."<<endl;
+    vector<int> arrOdd = {-2, 0, 3, 6, 7, 8, 9};
+    vector<int> arrEven = {-2, 0, 3, 7, 8, 9, 12, 15};
+
+    int ans = BinarySearch(arrEven, target);
+    cout << "the target " << target << " is exists at " << ans << "th Index on array." << endl;
 }

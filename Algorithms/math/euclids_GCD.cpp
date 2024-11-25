1c2074da13aaf6bfc32851856c5c28ef429ccfb3
RohitDudi <rohitdudijat@yahoo.com>
2024-11-25 10:00:16 +0530

    Euclid's Algorithm for GCD

---
+++ Algorithms/math/euclids_GCD.cpp
@@ -0,0 +1,33 @@
+// euclid's Algorithm to find GCD
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+        {
+            a = a % b;
+        }
+        else
+        {
+            b = b % a;
+        }
+    }
+    if (a == 0)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
+
+int main()
+{
+    int a = 28, b = 20;
+    cout << "the Greatest common deviser of " << a << " and " << b << " is: " << GCD(a, b) << endl;
+    return 0;
+}
